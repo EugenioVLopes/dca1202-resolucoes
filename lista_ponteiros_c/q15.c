2bc139f3df29c0ac551fe39fe88c4cdcf6d6e9f8
EUGENIO SANTOS <eugenio.lopes.090@ufrn.edu.br>
2024-03-22 09:17:29 -0300

    add resolucao da q15

--- lista_ponteiros_c/q15.c
+++ lista_ponteiros_c/q15.c
@@ -5,6 +5,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    return 0 
+int main()
+{
+    int n; // Variável que receberá o tamanho do vetor
+
+    float *vetor; // Ponteiro para o vetor
+
+    printf("Digite o tamanho do vetor: ");
+
+    scanf("%d", &n); // Lendo o tamanho do vetor por meio do número dado pelo usuário
+
+    vetor = (float *)malloc(n * sizeof(float)); // Alocando memória para o vetor
+
+    // Verificando se a memória foi alocada corretamente
+    if (vetor == NULL)
+    {
+        printf("Memória insuficiente\n");
+        return 1;
+    }
+
+    // Lendo os valores do vetor
+    for (int i = 0; i < n; i++)
+    {
+        printf("Digite o valor %d: ", i + 1);
+        scanf("%f", &vetor[i]);
+    }
+
+    // Ordenando o vetor
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (vetor[i] > vetor[j])
+            {
+                float aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+            }
+        }
+    }
+    printf("Vetor ordenado: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%.2f ", vetor[i]);
+    }
+
+    free(vetor); // Liberando a memória alocada para o vetor
+
+    vetor = NULL; // Atribuindo NULL ao ponteiro vetor
+
+    return 0;
 }
